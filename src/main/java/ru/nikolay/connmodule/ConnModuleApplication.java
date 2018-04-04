930d3020f79cb04c0a880c131b98d87f213bbbb4
nikolay <schaiydulin@outlook.com>
2018-04-04 15:50:32 +0300

    init commit

---
+++ src/main/java/ru/nikolay/connmodule/ConnModuleApplication.java
@@ -0,0 +1,12 @@
+package ru.nikolay.connmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnModuleApplication.class, args);
+    }
+}
